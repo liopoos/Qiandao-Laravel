cea25e6069bd0581f75fc9162fc4cabe0cb30377
shaoshuai <i@mayuko.cn>
2019-04-04 10:21:12 +0800

    perf: 优化样式

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') - {{config('app.name')}}</title>
-    <link href="{{ asset('css/bootstrap.main.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.yeti.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
     <link rel="shortcut icon" href="{{asset('/favicon.ico')}}">
